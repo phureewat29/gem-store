44a9a7cd33a8041041d9f6200254a36d754c308f
Phureewat A <a.phureewat@gmail.com>
2023-11-11 04:45:13 +0700

    misc: common libs

---
+++ libs/common/src/filter/http-exception.filter.ts
@@ -0,0 +1,19 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const status = exception.getStatus();
+    const data = exception.getResponse();
+
+    return response.status(status).json(data);
+  }
+}
